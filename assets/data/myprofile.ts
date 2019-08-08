6867712b2a675cd6e9496dfac7ae92f17a50f559
ishanDahiya <52500042+ishanDahiya@users.noreply.github.com>
2019-08-07 23:51:58 -0400

    Add files via upload

---
+++ assets/data/myprofile.ts
@@ -0,0 +1,9 @@
+import { dahiyai } from '../../app/dahiyai';
+
+export const MYPROFILE:dahiyai = {
+    snumber: 991485199,
+    sname: 'Ishan Dahiya',
+    slogin: 'dahiyai',
+    scampus: 'Davis Campus',
+    stitle: 'Assignment 4'
+}
